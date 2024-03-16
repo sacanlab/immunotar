e47fa3de270317883d49a40fa0d67d7117a449e0
Ahmet Sacan <ahmetdevel@umich.edu>
2024-03-15 20:15:01 -0400

    initial build

--- R/immunotar_makepackage.r
+++ R/immunotar_makepackage.r
@@ -11,7 +11,7 @@ immunotar_makepackage=function(...,dryrun=F,echo=T){
   	pkgname='immunotar'
   	,rsync_exclude_more=arr_csv('Archive/,shinyapp/,sandbox.r,sandbox.rmd,packager.r,packager_demo.rmd,mysetwd.r,config.*.yml,git.r')
   	,rsyncmore=list(
-  		list(src=io_name(thisdir(),'../data/'),dest='inst/data/', include=c('demo*','optimized*','project_defaultparams.yml') )
+  		list(src=io_name(thisdir(),'../data/'),dest='inst/data/', include=c('demo*','optimized*','project_defaultparams.yml','project_optimizedparams.yml') )
   		,list(src=io_name(thisdir()),dest='inst/', include=c('config.yml') )
   		)
   ,...);
